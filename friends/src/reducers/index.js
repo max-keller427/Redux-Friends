dad4ccbd8fa9c3d2b563e4a7e522e6da7baa47c3
max keller <max.keller427@gmail.com>
2019-04-19 12:49:43 -0600

    built out reducer

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,13 +1,69 @@
+import {
+ LOGIN_START 
+ LOGIN_SUCCESS 
+ LOGIN_FAILURE 
+ FRIENDS_START 
+ FRIENDS_SUCCESS 
+ FRIENDS_FAILURE
+ ADD_START 
+ ADD_SUCCESS 
+ ADD_FAILURE 
+} from '../actions';
+
 const initialState = {
-  error: "",
-  fetchingData: false,
-  gasPrices: []
+    deletingFriend: false,
+    fetchingFriends: false,
+    friends: [],
+    loggingIn: false,
+    savingFriends: false,
+    updatingFriend: false,
+    error: null
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    default:
-      return state;
+    case LOGIN_START:  {
+        return {
+            ...state,
+            error: '',
+            loggingIn: true,
+        }
+    }
+    case LOGIN_SUCCESS : {
+        return {
+            ...state,
+            loggingIn: false,
+        }
+    }
+    case LOGIN_FAILURE: {
+        return {
+            ...state,
+            error: 'Login Failed'
+            loggingIn: false
+        }
+    }
+    case ADD_START: {
+        return {
+            ...state,
+            error: '',
+            fetchingFriends: true;
+        }
+    }
+    case ADD_SUCCESS: {
+        console.log(action.payload)
+        return {
+            ...state,
+            error: '',
+            fetchingFriends: false,
+            friends: action.payload
+        }
+    }
+    case ADD_FAILURE: {
+        return {
+            ...state,
+            error: action.payload.status
+        }
+    }
   }
 };
 
