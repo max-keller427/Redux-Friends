cb9bfa1829ae784e0d5b746f7e1a8a7f07939a97
max keller <max.keller427@gmail.com>
2019-04-19 12:27:21 -0600

    built out action page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import Login from "./components/Login";
 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <Login />
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Route path="/login" component={Login} />
+        <Route exact path="/protected" component={} />
       </div>
+    </Router>
     );
   }
 }
---
+++ friends/src/actions/index.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+export const LOGIN_START = "LOGIN_START";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+
+export const FRIENDS_START = "FRIENDS_START";
+export const FRIENDS_SUCCESS = "FRIENDS_SUCCESS";
+export const FRIENDS_FAILURE = "FRIENDS_FAILURE";
+
+export const ADD_START = "ADD_START";
+export const ADD_SUCCESS = "ADD_SUCCESS";
+export const ADD_FAILURE = "ADD_FAILURE";
+
+export const login = creds => dispatch => {
+  dispatch({ type: LOGIN_START });
+
+  return axios.post("http://localhost:5000/api/login", creds).then(res => {
+    localStorage.setItem("token", res.data.payload);
+    dispatch({ type: LOGIN_SUCCESS });
+  });
+};
+
+export const getFriendsData = () => dispatch => {
+  dispatch({ type: FRIENDS_START });
+  axios
+    .get("http://localhost5000/api/data", {
+      headers: { Authorization: localStorage.getItem("token") }
+    })
+    .then(res => {
+      console.log(res);
+      dispatch({
+        type: FRIENDS_SUCCESS,
+        payload: res.data.data
+      });
+    });
+    .catch(err => {
+      console.log(err.response);
+      if (err.response.status === 403) {
+        localStorage.removeItem('token');
+      }
+      dispatch( {type: FRIENDS_FAILURE, payload: err.response} )
+    })
+};
